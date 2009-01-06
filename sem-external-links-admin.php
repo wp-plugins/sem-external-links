7c9877d3f984dee173130a18da35baff8f3b7137
Denis-de-Bernardy <Denis-de-Bernardy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-06 11:51:17 +0000

    sem-external-links 3.0.4
    git-svn-id: https://plugins.svn.wordpress.org/sem-external-links/trunk@84535 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sem-external-links-admin.php
+++ sem-external-links-admin.php
@@ -95,7 +95,7 @@ class external_links_admin
 				. ( $options['global'] ? ' checked="checked"' : '' )
 				. ' />'
 			. '&nbsp;'
-			. __('Process all outbound links as configured below. This means links in the sidebars, header, footer and so on in addition to those in posts\' and pages\' content. Note: If you add a nofollow attribute, be sure to turn this off if you wish to let your commenters to have some Google Juice.')
+			. __('Process all outbound links as configured below. This means links in the sidebars, header, footer and so on in addition to those in posts\' and pages\' content. Note: If you add a nofollow attribute, be sure to turn this off if you wish to let your commenters to have some Google Juice. Note: In the <a href="http://www.semiologic.com/software/wp-themes/">Semiologic Reloaded theme</a>, icons are not added outside of the main area.')
 			. '</label>'
 			. '</td>'
 			. '</tr>';
--- sem-external-links.php
+++ sem-external-links.php
@@ -4,7 +4,7 @@ Plugin Name: External Links
 Plugin URI: http://www.semiologic.com/software/publishing/external-links/
 Description: Adds a class=&quot;external&quot; to all outbound links, with various effects that are configurable under Options / External Links. Use &lt;a class=&quot;no_icon&quot; ...&gt; to disable the icon on individual links.
 Author: Denis de Bernardy
-Version: 3.0.3
+Version: 3.0.4
 Author URI: http://www.getsemiologic.com
 */
 
